a029a2e786237489e7a3f819282947aa0b713dcb
GalynaSergeevna <60966598+GalynaSergeevna@users.noreply.github.com>
2020-12-23 12:35:05 +0200

    Coronavirus Tracker *Based on tutorial https://www.youtube.com/watch?v=8hjNG9GZGnQ
    Spring Boot Java application to track coronavirus confirmed cases across the globe.

---
+++ coronavirus-tracker/src/main/java/github/Sergeevna/coronavirustracker/controllers/HomeController.java
@@ -0,0 +1,28 @@
+package github.Sergeevna.coronavirustracker.controllers;
+
+import github.Sergeevna.coronavirustracker.models.LocationStats;
+import github.Sergeevna.coronavirustracker.services.CoronaVirusDataService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    CoronaVirusDataService CoronaVirusDataService;
+
+    @GetMapping("/")
+    public String home(Model model) {
+        List<LocationStats> allStats = CoronaVirusDataService.getAllStats();
+        int totalReportedCases = allStats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
+        int totalNewCases = allStats.stream().mapToInt(stat -> stat.getDiffFromPrevDay()).sum();
+        model.addAttribute("locationStats", allStats);
+        model.addAttribute("totalReportedCases", totalReportedCases);
+        model.addAttribute("totalNewCases", totalNewCases);
+        return "home";
+    }
+}
